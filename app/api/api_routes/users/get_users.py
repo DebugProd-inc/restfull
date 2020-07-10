ca87e1b4a93945cb821f722f1eb239b624217b20
Flarinli <maxim.v.filatov@outlook.com>
2020-07-10 11:39:36 +0300

    realized routes for User, realized errors file for api, add utils for DB models

--- app/api/api_routes/users/get_users.py
+++ app/api/api_routes/users/get_users.py
@@ -1,6 +1,11 @@
+from flask import request, jsonify
+from app.models.user import User
 from app.api import bp
 
 
 @bp.route('/users', methods=['GET'])
 def get_users():
-    pass
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = User.to_collection_dict(User.query, page, per_page, 'api.get_users')
+    return jsonify(data)
