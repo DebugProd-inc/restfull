9ab9d0f82f5601dfea405452ea880fb7f645471e
KatyaGorina <katushu2000@mail.ru>
2020-07-11 13:02:17 +0300

    added imports and center calculation

--- math/math.py
+++ math/math.py
@@ -1,1 +1,25 @@
-# this for Anton, he love math
+import numpy as np
+import scipy
+import scipy.spatial
+
+# test values of implementation
+x1 = np.array([0.5, 0.3, 0.5, 0.8])
+x2 = np.array([0.1, 0.3, 0.5, 0.3])
+x3 = np.array([0.3, 0.1, 0.1, 0.1])
+
+implementation_values = np.row_stack((x1, x2, x3))
+
+
+def center(n, x_i):
+    result = np.zeros(n)
+    i = 0
+    for row in x_i:
+        result[i]+=sum(row)/n
+        i += 1
+    return result
+
+
+x_c = center(len(implementation_values), implementation_values)
+
+print("X_c = ", x_c)
+
