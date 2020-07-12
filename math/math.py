f920c760b6f06f927454a81b2baacaa36302c8d7
KatyaGorina <katushu2000@mail.ru>
2020-07-12 15:47:44 +0300

    Added mahalanobis distance calculation function

--- math/math.py
+++ math/math.py
@@ -1,12 +1,15 @@
+# from __future__ import division
 import numpy as np
+import random
 import scipy
 import scipy.spatial
 
 # test values of implementation
-x1 = np.array([0.5, 0.3, 0.5, 0.8])
-x2 = np.array([0.1, 0.3, 0.5, 0.3])
-x3 = np.array([0.3, 0.1, 0.1, 0.1])
+x1 = np.array([random.gauss(2, 4), random.gauss(2, 4), random.gauss(2, 4), random.gauss(2, 4), random.gauss(2, 4)])
+x2 = np.array([random.gauss(2, 4), random.gauss(2, 4), random.gauss(2, 4), random.gauss(2, 4), random.gauss(2, 4)])
+x3 = np.array([random.gauss(2, 4), random.gauss(2, 4), random.gauss(2, 4), random.gauss(2, 4), random.gauss(2, 4)])
 
+# переменная для хранения массивов начальных значений
 implementation_values = np.row_stack((x1, x2, x3))
 
 
@@ -21,5 +24,18 @@ def center(n, x_i):
 
 x_c = center(len(implementation_values), implementation_values)
 
-print("X_c = ", x_c)
 
+def mahalanob(x_i):
+    result = np.zeros(len(x_i[0]))
+    cov_matrix = np.cov(implementation_values)
+    invers_cov_matrix = np.linalg.inv(cov_matrix)
+    i = 0
+    for i in range(0, len(x_i[0])):
+        result[i] = scipy.spatial.distance.mahalanobis(x_i[:, i], x_c, invers_cov_matrix)
+        i=i+1
+    return result
+
+
+mahalanob_test = mahalanob(implementation_values)
+
+print("M_D = ", mahalanob_test)
