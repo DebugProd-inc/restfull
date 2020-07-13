028fd271a23ee403758b8ac560bf1d6784bf740d
ansiane <vip.nastenk00@mail.ru>
2020-07-13 17:57:04 +0300

    add table structure

--- app/models/subsystem.py
+++
@@ -1,6 +0,0 @@
-from flask import url_for
-from app import db
-
-
-class Subsystem(db.Model):
-    pass
---
+++ app/models/system.py
@@ -0,0 +1,10 @@
+from flask import url_for
+from app import db
+
+
+class System(db.Model):
+    __tablename__ = "systems"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    id_board = db.Column(db.Integer, db.ForeignKey("boards.id"))
+    board = db.relationship("Board")
