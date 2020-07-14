e4dac5e5d3b607532743fc5c6bf843502240dbed
Nikitos-div <motiki839@gmail.com>
2020-07-14 18:40:26 +0300

    added headers and resolve problem

--- app/api/api_routes/users/create_user.py
+++ app/api/api_routes/users/create_user.py
@@ -5,7 +5,7 @@ from app.api import bp
 from app.api.errors import bad_request
 
 
-@bp.route('/users', methods=['POST'])
+@bp.route('/createuser', methods=['POST'])
 def create_user():
     data = request.get_json() or {}
     if 'username' not in data or 'email' not in data or 'password' not in data:
@@ -19,32 +19,34 @@ def create_user():
     db.session.add(user)
     db.session.commit()
     response = jsonify(user.to_dict())
+    response
     response.status_code = 201
     response.headers['Location'] = url_for('api.get_user', id=user.id)
-    # response.headers['Access-Control-Allow-Origin'] = '*'
-    # response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE"
-    # response.headers["Access-Control-Allow-Headers"] = \
-    # "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = \
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    print(response.headers)
     return response
 
 
-@bp.before_request
-def before_request(response):
-    white_origin = ['http://localhost:8080',
-                    'https://debug-product-test.web.app']
-    if request.headers['Origin'] in white_origin:
-        response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
-        response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-    return response
+# @bp.before_request
+# def before_request(response):
+#     white_origin = ['http://localhost:8080',
+#                     'https://debug-product-test.web.app']
+#     if request.headers['Origin'] in white_origin:
+#         response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+#         response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+#         response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+#     return response
 
 
-@bp.after_request
-def after_request(response):
-    white_origin = ['http://localhost:8080',
-                    'https://debug-product-test.web.app']
-    if request.headers['Origin'] in white_origin:
-        response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
-        response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-    return response
+# @bp.after_request
+# def after_request(response):
+#     white_origin = ['http://localhost:8080',
+#                     'https://debug-product-test.web.app']
+#     if request.headers['Origin'] in white_origin:
+#         response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+#         response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+#         response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+#     return response
