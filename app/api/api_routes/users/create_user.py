58591e64406454f5164f8d8bd32c351660c4aeb7
Nikitos-div <motiki839@gmail.com>
2020-07-14 23:58:41 +0300

    add json to create user

--- app/api/api_routes/users/create_user.py
+++ app/api/api_routes/users/create_user.py
@@ -3,11 +3,14 @@ from app import db
 from app.models.user import User
 from app.api import bp
 from app.api.errors import bad_request
+import json
 
 
 @bp.route('/user', methods=['POST'])
 def create_user():
-    data = request.get_json() or {}
+    data = json.loads(request.get_data()) or {}
+    print(111111111111111111111, 'email' in data,
+          'username' in data, 'password' in data)
     if 'username' not in data or 'email' not in data or 'password' not in data:
         return bad_request('must include username, email and password fields')
     if User.query.filter_by(username=data['username']).first():
@@ -19,7 +22,6 @@ def create_user():
     db.session.add(user)
     db.session.commit()
     response = jsonify(user.to_dict())
-    response
     response.status_code = 201
     response.headers['Location'] = url_for('api.get_user', id=user.id)
     return response
