5418cc61b789a72e1eae55b2a81b4ecdd8ec39f8
KatyaGorina <katushu2000@mail.ru>
2020-07-14 11:55:18 +0300

    dispersion calculation added

--- math/math.py
+++ math/math.py
@@ -27,7 +27,7 @@ x3 = np.array([
     ])
 
 # переменная для хранения массивов начальных значений
-implementation_values = np.row_stack((x1, x2, x3))
+IMPLEMENTATION_VALUES = np.row_stack((x1, x2, x3))
 
 
 def center(x_i):
@@ -39,14 +39,23 @@ def center(x_i):
     return result
 
 
-X_C = center(implementation_values)
+X_C = center(IMPLEMENTATION_VALUES)
+
+
+def dispersion(x_i):
+    i = 0
+    if len(x_i.shape) != 1:
+        result = np.var(x_i, axis=1)
+    else:
+        result = np.var(x_i)
+    return result
 
 
 def mahalanob(x_i):
     result = np.zeros(
         len(x_i[0])
         )
-    cov_matrix = np.cov(implementation_values)
+    cov_matrix = np.cov(IMPLEMENTATION_VALUES)
     invers_cov_matrix = np.linalg.inv(cov_matrix)
     i = 0
     for i in range(0, len(x_i[0])):
@@ -62,12 +71,11 @@ def getting_quantile(dj):
     return np.quantile(dj, confidence_probability)
 
 
-MU_MAX = getting_quantile(mahalanob(implementation_values))
+MU_MAX = getting_quantile(mahalanob(IMPLEMENTATION_VALUES))
 
 
 def functional_check(distance):
     if distance <= MU_MAX:
         return True
-
     else:
         return False
