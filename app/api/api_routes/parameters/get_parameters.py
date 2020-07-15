02e1432f8d689fe5c4f32301d595f153eaff6a78
ansiane <vip.nastenk00@mail.ru>
2020-07-15 14:56:10 +0300

    added new api_routes

---
+++ app/api/api_routes/parameters/get_parameters.py
@@ -0,0 +1,18 @@
+from flask import request, jsonify
+from app.models.parameter import Parameter
+from app.api import bp
+from app.api.auth import token_auth
+
+
+@bp.route('/parameters', methods=['GET'])
+@token_auth.login_required
+def get_parameters():
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = Parameter.to_collection_dict(
+        Parameter.query,
+        page,
+        per_page,
+        'api.get_parameters'
+    )
+    return jsonify(data)
