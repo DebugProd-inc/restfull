924670c3ffce829106f9e6ca1bbe37109aa9e4f1
KatyaGorina <katushu2000@mail.ru>
2020-07-21 13:58:41 +0300

    some bugs fixed but not all

--- math/TV_i_MS.py
+++ math/TV_i_MS.py
@@ -2,11 +2,11 @@ import numpy as np
 import scipy
 import scipy.spatial
 import math
-# from test_values import confidence_probability
+
 
 # получение из выборки расстояний Махаланобиса при штатном
 # функционировании квантили заднного уровня
-S = 0  # степень полинома я хз как её реально найти, так что от балды
+S = 0  # степень полинома в распределении Вейбулла
 
 
 # квантиль выборочной функции распределения
@@ -16,7 +16,9 @@ def get_quantile(dj, confidence_probability):
 
 # МНК оценка коэфициентов
 def parameter_estimation(X, Y):
+    print(Y)
     X_ = X.transpose()
+    Y = np.matrix(Y)
     result = np.dot(X_, X)
     result = np.linalg.inv(result)
     result = np.dot(result, X_)
@@ -41,25 +43,26 @@ def get_X_Matrix(dj):
     New_d = []
     for i in range(0, len(dj)):
         New_d.append([])
-        for j in range(S + 1, 0):
+        for j in range(0, S + 1):
             New_d[i].append(dj[i]**j)
-    return New_d
+    return np.matrix(New_d)
 
 
 # Класс функции распределения
 class Class_distribution_func:
-    def __init__(self, dj):  # конструктор
-        self.d = dj.sort()
+    # конструктор
+    def __init__(self, dj):
         # запоминаем значения выборки дистанции (особо не надо)
-        fun = lambda x: (-(math.log(1-x)))  # ему не нравится лямбда
-        # но мне пофиг, потому что мне нравится)))))
-        B_ = map(fun, get_func(dj))  # по выборочной функции распределения
+        self.d = dj.sort()
+        # по выборочной функции распределения
         # находим значения полинома для каждого из значений функции
+        B_ = np.matrix(list(map(lambda x: (-(math.log(1-x))), get_func(dj))))
+        print(B_)
+        # находим коэффициенты полинома (чтобы потом вычислять)
         self.parameters = parameter_estimation(
             get_X_Matrix(dj),
             np.transpose(B_)
             )
-        # находим коэффициенты полинома (чтобы потом вычислять)
 
     def B_Veybull(self, x):  # значение полинома для распределения Вейбулла
         return np.polyval(self.parameters, x)
@@ -81,3 +84,9 @@ class Class_distribution_func:
         # фильтруем корни от отрицательных и комплексных
         return min(roots)  # выбор корня подвергается критике
         # я хз пока как делать правильно, так что пока так
+
+
+# значения для проверки функциональности
+# a = np.array([1, 1.1, 1.9, 2, 1.2, 2.1])
+# c = Class_distribution_func(a)
+# print(c.get_quantile(0.8))
